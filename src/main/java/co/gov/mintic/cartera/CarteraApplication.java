e3989a6d3d5af0f5a51557d1ee12ab5118846f02
Alvaro Uribe <adummis@hotmail.com>
2022-09-08 18:58:37 -0500

    Creación del proyecto inicial

---
+++ src/main/java/co/gov/mintic/cartera/CarteraApplication.java
@@ -0,0 +1,13 @@
+package co.gov.mintic.cartera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarteraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarteraApplication.class, args);
+	}
+
+}
